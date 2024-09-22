764c0440e49b5f816cf8f7401271ee5485ec952b
Luana Calisto <luanavenancio060@gmail.com>
2024-09-22 12:17:12 -0300

    shoppingcart Class, addToCart function, toHTml function and app.ts update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import { Product } from "./entities/product";
 import data from "../data.json";
 
-console.log(data[0].name)
+const products: Product[] = [];
+
+for (let i = 0; i < data.length; i++) {
+    const product = new Product(data.[i].name, data.[i].price, data.[i].category, data.[i].image);
+    products.push(product);
+
+    product.toHtml()
+}
